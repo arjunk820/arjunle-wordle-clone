f57b6a36c73dceb48d924a23eba12c6e62828c96
arjun <arjunkan2003@gmail.com>
2025-04-02 18:59:07 -0400

    feat: wordle functionality active

--- build/script.js
+++ build/script.js
@@ -3,9 +3,9 @@ import { WORDS } from "./words.js";
 const MAX_GUESSES = 6;
 let remGuesses = MAX_GUESSES;
 let currGuess = [];
-let nextLetter = 0;
-let rightGuessString = WORDS[Math.floor(Math.random() * WORDS.length)]
-console.log(rightGuessString)
+let indexLetter = 0;
+let correctAnswer = WORDS[Math.floor(Math.random() * WORDS.length)]
+console.log(correctAnswer)
 
 function initBoard() {
     let board = document.getElementById("game-board");
@@ -26,3 +26,157 @@ function initBoard() {
 }
 
 initBoard()
+
+// insertLetter - expects a letter to be pressed to be inserted into the
+// row corresponding to the current guess
+function insertLetter (pressedKey) {
+    if (indexLetter === 5) {
+        return
+    }
+    pressedKey = pressedKey.toLowerCase()
+
+    let row = document.getElementsByClassName("letter-row")[6 - remGuesses]
+    let box = row.children[indexLetter]
+    box.textContent = pressedKey
+    box.classList.add("filled-box")
+    currGuess.push(pressedKey)
+    indexLetter += 1
+}
+
+// deleteLetter - deletes the last letter pressed from the current guess (row)
+function deleteLetter () {
+    let row = document.getElementsByClassName("letter-row")[6 - remGuesses]
+    let box = row.children[indexLetter - 1]
+    box.textContent = ""
+    box.classList.remove("filled-box")
+    currGuess.pop()
+    indexLetter -= 1
+}
+
+// shadeKeyboard - shades the keyboard yellow/green/grey
+function shadeKeyboard(letter, color) {
+    for (const elem of document.getElementsByClassName("keyboard-button")) {
+        if (elem.textContent === letter) {
+            let oldColor = elem.style.backgroundColor
+            if (oldColor === 'green') {
+                return
+            } 
+
+            if (oldColor === 'yellow' && color !== 'green') {
+                return
+            }
+
+            elem.style.backgroundColor = color
+            break
+        }
+    }
+}
+
+// checkGuess - goes through the user's guess and checks letter by letter
+// according to the rules of Wordle
+function checkGuess () {
+    let row = document.getElementsByClassName("letter-row")[6 - remGuesses]
+    let guessString = ''
+    let rightGuess = Array.from(correctAnswer)
+
+    for (const val of currGuess) {
+        guessString += val
+    }
+
+    if (guessString.length != 5) {
+        toastr.error("Not enough letters!")
+        return
+    }
+
+    if (!WORDS.includes(guessString)) {
+        toastr.error("Word not in list!")
+        return
+    }
+
+    for (let i = 0; i < 5; i++) {
+        let letterColor = ''
+        let box = row.children[i]
+        let letter = currGuess[i]
+
+        let letterPosition = rightGuess.indexOf(currGuess[i])
+        // is letter in the correct guess
+        if (letterPosition === -1) {
+            letterColor = 'grey'
+        } else {
+            // now, letter is definitely in word
+            // if letter index and right guess index are the same
+            // letter is in the right position 
+            if (currGuess[i] === rightGuess[i]) {
+                // shade green 
+                letterColor = 'green'
+            } else {
+                // shade box yellow
+                letterColor = 'yellow'
+            }
+
+            rightGuess[letterPosition] = "#"
+        }
+
+        let delay = 250 * i
+        setTimeout(()=> {
+            // shade box
+            box.style.backgroundColor = letterColor
+            shadeKeyboard(letter, letterColor)
+        }, delay)
+    }
+
+    if (guessString === correctAnswer) {
+        toastr.success(`You got it in "${remGuesses}!"`)
+        remGuesses = 0
+        return
+    } else {
+        remGuesses -= 1;
+        currGuess = [];
+        indexLetter = 0;
+
+        if (remGuesses === 0) {
+            toastr.error("You've run out of guesses! Game over!")
+            toastr.info(`The right word was: "${correctAnswer}"`)
+        }
+    }
+}
+
+document.addEventListener("keyup", (e) => {
+
+    if (remGuesses === 0) {
+        return
+    }
+
+    let pressedKey = String(e.key)
+    if (pressedKey === "Backspace" && indexLetter !== 0) {
+        deleteLetter()
+        return
+    }
+
+    if (pressedKey === "Enter") {
+        checkGuess()
+        return
+    }
+
+    let found = pressedKey.match(/[a-z]/gi)
+    if (!found || found.length > 1) {
+        return
+    } else {
+        insertLetter(pressedKey)
+    }
+})
+
+document.getElementById("keyboard-cont").addEventListener("click", (e) => {
+    const target = e.target
+
+    if (!target.classList.contains("keyboard-button")) {
+        return
+    }
+    let key = target.textContent
+
+    if (key === "Del") {
+        key = "Backspace"
+    } 
+
+    document.dispatchEvent(new KeyboardEvent("keyup", {'key': key}))
+})
