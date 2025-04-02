7be874f3648e9478d21007090eaf6c714fe4b184
arjun <arjunkan2003@gmail.com>
2025-04-02 14:53:57 -0400

    began creating game board

--- build/script.js
+++ build/script.js
@@ -6,3 +6,23 @@ let currGuess = [];
 let nextLetter = 0;
 let rightGuessString = WORDS[Math.floor(Math.random() * WORDS.length)]
 console.log(rightGuessString)
+
+function initBoard() {
+    let board = document.getElementById("game-board");
+
+    // Creates the 6 x 5 game board.
+    for (let i = 0; i < MAX_GUESSES; i++) {
+        let row = document.createElement("div")
+        row.className = "letter-row"
+
+        for (let j = 0; j < 5; j++) {
+            let box = document.createElement("div")
+            box.className = "letter-box"
+            row.appendChild(box)
+        }
+
+        board.appendChild(row)
+    }
+}
+
+initBoard()
